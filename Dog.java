975c624e45f691af5e55424c1edc8b060081c341
Davino Heyse <davino.heyse@campuskompas.be>
2023-11-06 11:42:23 +0100

    t message
    e

---
+++ Dog.java
@@ -0,0 +1,11 @@
+public class Dog {
+    String name;
+    String breed;
+    String color;
+    String arrivalTime;
+    String sex;
+    int age;
+    boolean canMultiply;
+    boolean hasVaccin;
+    boolean isAlive;
+}
---
+++ DogHotel.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class DogHotel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welkom in het hondenhotel!");
+        System.out.println("Wat wilt u doen?");
+        System.out.println("1. hond toevoegen.");
+        System.out.println("2. hond verwijderen.");
+        System.out.println("3. Afsluiten.");
+        int firstAnswer = scanner.nextInt();
+        if (firstAnswer == 1) {
+            System.out.println("test");
+        }
+        if (firstAnswer == 2) {
+                System.out.println("tes6768t");
+        }
+        if (firstAnswer == 3) {
+                    System.out.println("te78t");
+        }
+
+
+
+
+
+    }
+}
